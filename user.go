1ffcd36cc5aebae891f1a346b132d04854159430
Nikolay Dementyev <you@example.com>
2023-01-30 18:05:44 +0400

    adds User struct

---
+++ user.go
@@ -0,0 +1,15 @@
+package go_graphql
+
+import "time"
+
+type UserRepo interface {
+}
+
+type User struct {
+	ID        string
+	Username  string
+	Email     string
+	Password  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
